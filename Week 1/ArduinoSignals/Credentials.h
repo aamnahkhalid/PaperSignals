6f5fceca99bd7fa7528048f6e253fe5b9576a2be
aamnah <aamnahkhalid@DN0a1f2e22.SUNet>
2018-02-23 13:58:11 -0800

    Week 7 Commit

---
+++ Week 1/ArduinoSignals/Credentials.h
@@ -0,0 +1,34 @@
+
+
+/*
+Copyright 2017 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. 
+*/
+
+/* WiFi Network must be 2.4GHz */
+const String SSID = "AndroidAP"; 
+const String Password = "wallacegotdis";
+
+/* Your Signal ID should be lowercase, and each word should be separated by dashes. 
+If Google Assistant spoke too quickly, you can ask to repeat the identifier at any 
+time be saying "What is the setup code for [SIGNAL NAME]?" */
+const String SignalID = "joshingly-valid-walrus";
+
+/* Enter the below API keys to track the weather on Umbrella Signal and Pants Signal. */
+const String DarkSkyAPIKey = "f24cd6035538b933026b4828ae8b9dbb";
+const String GeoCodingAPiKey = "AIzaSyD3yiLwitj4ahEEUJo39DorYnEkc3uTSjw";
+
+/* Enter the below API key to track stocks on Arrow Signal. */
+const String BarChartAPIKey = "YOUR_BAR_CHART_API_KEY";
+
